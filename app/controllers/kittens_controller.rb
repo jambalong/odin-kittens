820709f544f0dca2ff1699e8ebd1289f3a684066
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-13 14:28:50 -0700

    Add flash notices and alerts respectively

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:notice] = "Congratz Bruh! You just added a new kitten"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Oopsies! You made an error with your lil kitten"
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +31,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:notice] = "Wowie! You know how to edit a kitten"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Uhmmm... Something went wrong with your edit"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -39,6 +43,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
 
+    flash[:notice] = "Noooo! How dare you delete the little kitten"
     redirect_to root_path, status: :see_other
   end
 
